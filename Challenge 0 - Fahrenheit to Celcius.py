8b4834caaea9b4991e4583682794286847d7742d
Kinjai581 <125725410+Kinjai581@users.noreply.github.com>
2023-02-18 22:18:00 +1100

    Update Challenge 0 - Fahrenheit to Celcius.py

--- Challenge 0 - Fahrenheit to Celcius.py
+++ Challenge 0 - Fahrenheit to Celcius.py
@@ -1,23 +1,22 @@
 # You need to define your functions first using the formula in order for them to work.
 
-def convertToFahrenheit(degreesCelcius):
-    celciusConvert = degreesCelcius * (9/5) + 32
-    return celciusConvert
+def convertToFahrenheit(degreesCelsius):
+    celsiusConvert = degreesCelsius * (9/5) + 32
+    return celsiusConvert
 
-
-def convertToCelcuis(degreesFahrenheit):
+def convertToCelsuis(degreesFahrenheit):
     fahrenheitConvert = (degreesFahrenheit - 32) * 5/9
     return fahrenheitConvert
  
 # Note: This code was copied from the exercise section of the document. The assert keyword allows you to test if a condition in your code returns True. 
 # If it isn't, the program will raise an assertion error.
-assert convertToCelcuis(0) == -17.77777777777778
-assert convertToCelcuis(180) == 82.22222222222223
+assert convertToCelsuis(0) == -17.77777777777778
+assert convertToCelsuis(180) == 82.22222222222223
 assert convertToFahrenheit(0) == 32
 assert convertToFahrenheit(100) == 212
-assert convertToCelcuis(convertToFahrenheit(15)) == 15
+assert convertToCelsuis(convertToFahrenheit(15)) == 15
     
-degreesCelcius = int(input("Input degrees Celcuis: "))
-print(f"Fahrenheit output: {convertToFahrenheit(degreesCelcius)}") # Prints the inputed amount of celcius in fahrenheit.
+degreesCelsius = int(input("Input degrees Celsius: "))
+print(f"Fahrenheit output: {convertToFahrenheit(degreesCelsius)}") # Prints the inputed amount of celsius in fahrenheit.
 degreesFahrenheit = int(input("Input degrees Fahrenheit: "))
-print(f"Celcuis output: {convertToCelcuis(degreesFahrenheit)}") # Prints the inputed amount of celcius in fahrenheit. 
+print(f"Celsius output: {convertToCelsius(degreesFahrenheit)}") # Prints the inputed amount of celsius in fahrenheit. 
